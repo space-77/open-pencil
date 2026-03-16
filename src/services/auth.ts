d228d734e524cb03117396f3fd18126f0f6c91f4
李燃东 <lirandong@maxrocky.com>
2026-03-16 20:24:41 +0800

    "feat: update api files (doc2ts auto commit)."

---
+++ src/services/auth.ts
@@ -0,0 +1,40 @@
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA DOC2TS                        ##
+ * ##                                                           ##
+ * ## AUTHOR: space-77                                          ##
+ * ## SOURCE: https://github.com/space-77/doc2ts                ##
+ * ---------------------------------------------------------------
+ */
+
+import type { DocReqConfig } from "doc2ts";
+import ApiClient from "./client";
+import type { Auth as types, __common__ } from "./types";
+/**
+ * @name Auth
+ * @description Auth
+ */
+export default class Auth extends ApiClient {
+  /**
+   * @summary 用户登录
+   * @description 用户登录获取认证令牌
+   */
+  postLogin(body: __common__.InternalhandlerLoginRequest) {
+    const config: DocReqConfig = { url: "/auth/login", body, method: "post" };
+    return this.request<types.RPostLogin>(config);
+  }
+
+  /**
+   * @summary 用户注册
+   * @description 注册新用户账号
+   */
+  postRegister(body: __common__.InternalhandlerRegisterRequest) {
+    const config: DocReqConfig = {
+      url: "/auth/register",
+      body,
+      method: "post",
+    };
+    return this.request<types.RPostRegister>(config);
+  }
+}
+export const auth = new Auth();
---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+export { auth } from "./auth";
+export { images } from "./images";
+export { settings } from "./settings";
+export { documents } from "./documents";
+export { collaboration } from "./collaboration";
